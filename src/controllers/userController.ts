abc56e8867812de98e8604a8f3a91b30827fe2fa
mayank pandey <mayankpandey1099@gmail.com>
2024-06-05 18:46:47 +0530

    updated validation of input by effect in userController

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -2,18 +2,28 @@ import { Request, Response } from 'express';
 import { createNewUser } from '../services/userService';
 import {Effect} from "effect";
 
+
+const validateInput = (name: string, email: string): Effect.Effect<void, string> => {
+  if (!name || !email) {
+    return Effect.fail('Name and email are required');
+  } else {
+    return Effect.succeed(() => {});
+  }
+};
+
 export const createUserController = async (req: Request, res: Response)=> {
   const { name, email } = req.body;
 
-  // Validation: Check if all required fields are provided
-  if (!name || !email ) {
-    return res.status(400).json({ error: 'Name, email are required' });
-  }
   try {
+    await Effect.runPromise(validateInput(name, email));
     const effect = createNewUser(name, email);
     const result = await Effect.runPromise(effect);
     return res.status(201).json(result);
-  } catch (error) {
-    return res.status(500).json({ error: "Failed to create the user" });
+  }catch (error: any) {
+    if (error === 'Name and email are required') {
+      return res.status(400).json({error: "Name and email are required"});
+    }else {
+      return res.status(500).json({ error: "Failed to create the user" });
+    }
   }
 };
